2f777443b48bf70496aedb5a822a027da12d078d
Alex Dizer <Alex_D.2005@mail.ru>
2026-04-08 19:41:15 +0700

    2

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
+  port: parseInt(process.env.DB_PORT!) || 5432,
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'cardmarket',
